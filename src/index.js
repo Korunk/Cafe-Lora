3e267e19ec75bafc017e4baebeb9de0500fcb28e
Klára Chmelařová <korunka91@gmail.com>
2022-04-30 11:51:50 +0200

    menu JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('funguju!');
+
+const clickBtn = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+const iconElm = document.querySelector('#nav-btn');
+iconElm.addEventListener('click', clickBtn);
+
+const aFce = () => {
+  document.querySelector('nav').classList.add('nav-closed');
+};
+const aElms = document.querySelectorAll('nav a');
+aElms.forEach((item) => {
+  item.addEventListener('click', aFce);
+});
