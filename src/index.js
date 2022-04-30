ca2cae205017b2144d83c03b8432338b0d4d9688
Klára Chmelařová <korunka91@gmail.com>
2022-04-30 12:25:09 +0200

    zmena při objednání JS

--- src/index.js
+++ src/index.js
@@ -15,3 +15,22 @@ const aElms = document.querySelectorAll('nav a');
 aElms.forEach((item) => {
   item.addEventListener('click', aFce);
 });
+
+let ordered = true;
+const changeTitle = () => {
+  if (ordered === true) {
+    document.querySelector('.order-btn').textContent = 'Zrušit';
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    return (ordered = false);
+  }
+  if (ordered !== true) {
+    document.querySelector('.order-btn').textContent = 'Objednat';
+    document
+      .querySelector('.drink__cup')
+      .classList.remove('drink__cup--selected');
+    return (ordered = true);
+  }
+};
+const btnOrder = document
+  .querySelector('.order-btn')
+  .addEventListener('click', changeTitle);
